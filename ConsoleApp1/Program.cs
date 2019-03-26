1270d4533b6b9f16c1a4c879120032dcdd150e10
BetrandAntolim <antontheamor123@gmail.com>
2019-03-26 14:59:54 +0700

    Latihan3

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FakeTaxi baru = new FakeTaxi();
+
+            baru.DriverName = "Aoi";
+            baru.OnDuty = true;
+            baru.NumPassenger = 10;
+
+            baru.FakeTaxiInfo();
+            baru.PickUpPassenger();
+            baru.DropOffPassenger();
+            Console.ReadKey();
+        }
+    }
+}
